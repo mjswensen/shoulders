070a41bd8f918497df8cd721917cb9a154052f14
Matt Swensen <swensen.matthew@gmail.com>
2019-09-28 18:26:37 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'ink';
+
+import App from './ui';
+
+render(React.createElement(App));
---
+++ src/ui.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, Color } from 'ink';
+
+const App = () => (
+	<Text>
+		Hello, <Color green>World</Color>
+	</Text>
+);
+
+export default App;
